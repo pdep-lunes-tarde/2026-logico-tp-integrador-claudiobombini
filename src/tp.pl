7fd623e2620f34bcf078204f70ab4ae83d30ab13
f-baquero <fbaquero@frba.utn.edu.ar>
2026-08-01 21:13:30 -0300

    punto 1a

--- src/tp.pl
+++ src/tp.pl
@@ -1,3 +1,24 @@
 :- begin_tests(tpIntegrador, []).
 
+persona(denken, humano, 1290, auberst).
+persona(voll, enano, 1200, ende).
+persona(serie, elfa, 500, weise).
+persona(fern, humana, 1370, weise).
+persona(stark, humano, 1368, riegel).
+persona(lawine, humana, 1372, auberst).
+persona(kanne, humana, 1365, weise).
+persona(wirbel, humano, 1350, klares).
+persona(lernen, humano, 1315, auberst).
+persona(frieren, elfa, 100, weise).
+persona(eisen, enano, 1150, riegel).
+
+vivo()
+
+
+
+
+
+
+
+
 :- end_tests(tpIntegrador).
